9b9533061bb7d7f92817d92116219e43dc7ec9dd
Kabtamu Degifie <habtamu.admas12@gmail.com>
2022-09-18 08:40:06 +0300

    initial Commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { Provider } from 'react-redux';
-import { store } from './redux/store';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { PersistGate } from "redux-persist/integration/react";
+import "./index.css";
+import App from "./App";
+import { Provider } from "react-redux";
+import { store, persistor } from "./redux/store";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      Now, modify the
+      <PersistGate loading="null" persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
 import userReducer from "./slices/userSlice";
+import thunk from "redux-thunk";
 
-export const store =  configureStore({
-  reducer: userReducer,
-})
+const persistConfig = { key: "root", storage };
+
+const persistedReducer = persistReducer(persistConfig, userReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  devTools: process.env.NODE_ENV !== "production",
+  middleware: [thunk],
+});
+
+export const persistor = persistStore(store);
